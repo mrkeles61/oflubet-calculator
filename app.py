197579064afa8bdca1e590268cbdb31f9b3f0ca6
mrkeles61 <150115073+mrkeles61@users.noreply.github.com>
2024-08-18 19:42:10 +0000

    changed the placeholders

--- app.py
+++ app.py
@@ -3,47 +3,30 @@ from otuzbirmanyagi import entries
 
 app = Flask(__name__)
 
-inputs = {}
+inputs={}
 preset_num = None
 bonus_rates = {}
 match_name='AYIBOĞAN'
 winning_outcome= None
-oflubet_values = []
-sexbet_values = []
-maskulenbet_values = []
+second_winning_outcome=0
 
-# Define inputs_2d globally
-inputs_2d = []
-  # Initialize entries globally
+  
 
-
-def update_entries_with_inputs(entries, inputs_2d, bonus_rates):
-    site_names = ["oflu", "sex", "chad"]
-
-    for row in entries:
-        for entry in row:
-            site_index = entry['bet_site']
-
-            # Get the correct input value from the 2D array based on site_index and betted_outcome
-            input_value = inputs_2d[site_index][entry["betted_outcome"]]
-            entry['bet_Rate'] = input_value
-
-            # Get the bonus rate using the site name
-            site_name = site_names[site_index]
-            entry['bonus_orani'] = bonus_rates.get(site_name, 0)
-
-
-def calculate_second_day(entries, winning_outcome):
-    for row in entries:
-        for entry in row:
-            if entry['betted_outcome'] == (winning_outcome):
+def calculate_second_day(entries, winning_outcome,second_winning_outcome):
+    for row in range(3):
+        for col in range(3):
+            entry = entries[row][col]
+            if entry['betted_outcome'] == winning_outcome:
                 yatirilacak_tutar = entry['yatirilacak_tutar']
                 bet_rate = entry['bet_Rate']
                 cap_num = entry['cap_number']
                 bonus_orani = entry['bonus_orani']
+                
 
-                entry['second_day_YT'] = round((((yatirilacak_tutar * bonus_orani) / 10) - (cap_num / bet_rate) + 15),2)
-                                         
+                entry['second_day_YT'] = round((((yatirilacak_tutar * bonus_orani) / 10) - (cap_num / bet_rate) + 15), 2)
+                entries[row][col]['second_day_YT']=entry['second_day_YT']
+                entries[row][col]['total_winning']+=yatirilacak_tutar*bet_rate
+                
 
 
 def update_entries(preset_num, entries):
@@ -87,20 +70,57 @@ def update_entries(preset_num, entries):
         for col in range(3):
             bet_site = int(entries[row][col]['bet_site'])
             betted_outcome = int(entries[row][col]['betted_outcome'])
-            bet_rate = float(entries[row][col]['bet_Rate'])
+
+            if bet_site == 0:  # oflu
+                if betted_outcome == 0:
+                    bet_rate = float(request.form.get('oflu0', 0))
+                    entries[row][col]['bet_Rate']=bet_rate
+                elif betted_outcome == 1:
+                    bet_rate = float(request.form.get('oflu1', 0))
+                    entries[row][col]['bet_Rate']=bet_rate
+                elif betted_outcome == 2:
+                    bet_rate = float(request.form.get('oflu2', 0))
+                    entries[row][col]['bet_Rate']=bet_rate
+            elif bet_site == 1:  # sex
+                if betted_outcome == 0:
+                    bet_rate = float(request.form.get('sex0', 0))
+                    entries[row][col]['bet_Rate']=bet_rate
+                elif betted_outcome == 1:
+                    bet_rate = float(request.form.get('sex1', 0))
+                    entries[row][col]['bet_Rate']=bet_rate
+                elif betted_outcome == 2:
+                    bet_rate = float(request.form.get('sex2', 0))
+                    entries[row][col]['bet_Rate']=bet_rate
+            elif bet_site == 2:  # chad
+                if betted_outcome == 0:
+                    bet_rate = float(request.form.get('chad0', 0))
+                    entries[row][col]['bet_Rate']=bet_rate
+                elif betted_outcome == 1:
+                    bet_rate = float(request.form.get('chad1', 0))
+                    entries[row][col]['bet_Rate']=bet_rate
+                elif betted_outcome == 2:
+                    bet_rate = float(request.form.get('chad2', 0))
+                    entries[row][col]['bet_Rate']=bet_rate
+                    
             bonus_orani = 0
             cap_num = entries[row][col]['cap_number']
 
             # Assign bonus rate based on bet_site
             if bet_site == 0:
                 bonus_orani = oflu_bonus
+                entries[row][col]['bonus_orani']=bonus_orani
+
+                
             elif bet_site == 1:
                 bonus_orani = sex_bonus
+                entries[row][col]['bonus_orani']=bonus_orani
             elif bet_site == 2:
                 bonus_orani = chad_bonus
+                entries[row][col]['bonus_orani']=bonus_orani
 
             yatirilacak_tutar = (cap_num * 100) / (bet_rate * (100 + bonus_orani))
             entries[row][col]['yatirilacak_tutar']=round(yatirilacak_tutar,2)
+            entries[row][col]['total_winning']-=yatirilacak_tutar
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -109,86 +129,55 @@ def index():
     global preset_num
     global bonus_rates
     global winning_outcome
-    global inputs_2d
     global entries
     global match_name
+    global second_winning_outcome
+    
 
-    inputs = {
-        'oflu1': request.form.get('oflu1', ''),
-        'oflu0': request.form.get('oflu0', ''),
-        'oflu2': request.form.get('oflu2', ''),
-        'sex1': request.form.get('sex1', ''),
-        'sex0': request.form.get('sex0', ''),
-        'sex2': request.form.get('sex2', ''),
-        'chad1': request.form.get('chad1', ''),
-        'chad0': request.form.get('chad0', ''),
-        'chad2': request.form.get('chad2', ''),
-    }
-    bonus_rates = {
-        'oflu': request.form.get('oflubonus', ''),
-        'sex': request.form.get('sexbonus', ''),
-        'chad': request.form.get('chadbonus', '')
-    }
+    
+    
 
     if request.method == "POST":
-        # Debug print to see received form data
-        print("Form Data:", request.form)
-
-        # Retrieve preset_num from form
-        preset_num = int(request.form.get('table_type', 0))
-
-        # Retrieve bonus rates from form
+        
+        inputs = {
+            'oflu1': request.form.get('oflu1', ''),
+            'oflu0': request.form.get('oflu0', ''),
+            'oflu2': request.form.get('oflu2', ''),
+            'sex1': request.form.get('sex1', ''),
+            'sex0': request.form.get('sex0', ''),
+            'sex2': request.form.get('sex2', ''),
+            'chad1': request.form.get('chad1', ''),
+            'chad0': request.form.get('chad0', ''),
+            'chad2': request.form.get('chad2', ''),
+        }
         bonus_rates = {
-            'oflu': float(request.form.get('oflubonus', 0)),
-            'sex': float(request.form.get('sexbonus', 0)),
-            'chad': float(request.form.get('chadbonus', 0))
+            'oflu': request.form.get('oflubonus', ''),
+            'sex': request.form.get('sexbonus', ''),
+            'chad': request.form.get('chadbonus', '')
         }
 
-        # Initialize inputs_2d
-        inputs_2d = [
-            [
-                float(request.form.get('oflu0', 0)),
-                float(request.form.get('oflu1', 0)),
-                float(request.form.get('oflu2', 0))
-            ],
-            [
-                float(request.form.get('sex0', 0)),
-                float(request.form.get('sex1', 0)),
-                float(request.form.get('sex2', 0))
-            ],
-            [
-                float(request.form.get('chad0', 0)),
-                float(request.form.get('chad1', 0)),
-                float(request.form.get('chad2', 0))
-            ]
-        ]
 
-        # Update entries with bet rates and bonus rates
-        update_entries_with_inputs(entries, inputs_2d, bonus_rates)
+        # Retrieve preset_num from form
+        preset_num = int(request.form.get('table_type', 0))        
 
         # Retrieve winning outcome from form and debug print
-        winning_outcome_str = request.form.get('winning_outcome', '0')
-        print("Winning Outcome String:", winning_outcome_str)
-
-        try:
-            winning_outcome = int(winning_outcome_str)
-        except ValueError:
-            winning_outcome = 0  # Default value if conversion fails
-
-
+        winning_outcome = int(request.form.get('winning_outcome', '0'))
+    
         # Update entries based on the preset number
         update_entries(preset_num, entries)
-        calculate_second_day(entries, winning_outcome)
+        calculate_second_day(entries, winning_outcome,second_winning_outcome)
         match_name=request.form.get('match_name', 'default')
 
-        print("Match Name:", match_name)  # Debug print to check value
+        print(entries)
 
+        
     # Render the template with current values (no default fallback)
     return render_template('home.html',
                            preset_num=preset_num,
                            bonus_rates=bonus_rates,
                            inputs=inputs,
                            entries=entries,
+                           second_winning_outcome=second_winning_outcome,
                            winning_outcome=winning_outcome,
                            match_name=match_name)
 
--- otuzbirmanyagi.py
+++ otuzbirmanyagi.py
@@ -1,17 +1,18 @@
 entries = [
     [
-        {'bet_site': 0, 'betted_by': 'Mert', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 4000},
-        {'bet_site': 1, 'betted_by': 'Mert', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 4000},
-        {'bet_site': 2, 'betted_by': 'Mert', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 4000}
+        {'bet_site': 0, 'betted_by': 'Mert', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 4000,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 1, 'betted_by': 'Mert', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 4000,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 2, 'betted_by': 'Mert', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 4000,'second_winning_outcome': 0,'katlama_orani':0}
     ],
     [
-        {'bet_site': 0, 'betted_by': 'Keleş', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3950},
-        {'bet_site': 1, 'betted_by': 'Keleş', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3950},
-        {'bet_site': 2, 'betted_by': 'Keleş', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3950}
+        {'bet_site': 0, 'betted_by': 'Keleş', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3950,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 1, 'betted_by': 'Keleş', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3950,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 2, 'betted_by': 'Keleş', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3950,'second_winning_outcome': 0,'katlama_orani':0}
     ],
     [
-        {'bet_site': 0, 'betted_by': 'Semih', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3900},
-        {'bet_site': 1, 'betted_by': 'Semih', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3900},
-        {'bet_site': 2, 'betted_by': 'Semih', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3900}
+        {'bet_site': 0, 'betted_by': 'Yusuf', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3900,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 1, 'betted_by': 'Yusuf', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3900,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 2, 'betted_by': 'Yusuf', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3900,'second_winning_outcome':0,
+         'katlama_orani':0}
     ]
 ]
