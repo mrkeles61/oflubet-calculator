7ac25e1e7bdb532533ad6065b3e1d8e80ebfd01f
mrkeles61 <150115073+mrkeles61@users.noreply.github.com>
2024-08-22 16:16:20 +0000

    fixed both of the tables, made dynamic match name entry

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from erentemplates import entries_template,ilk_yatirma_template,kalan_cevrim_template
+from erentemplates import entries_template,ilk_yatirma_template,kalan_cevrim_template,entries_template_second
+import copy
 
 app = Flask(__name__)
 
@@ -19,7 +20,9 @@ def calculate_second_day(entries, winning_outcome):
 
                 entry['second_day_YT'] = round((((yatirilacak_tutar * bonus_orani) / 10) - (cap_num / bet_rate) + 15), 2)
                 entries[row][col]['second_day_YT']=entry['second_day_YT']
-                
+
+
+    
 
 
 def update_entries(preset_num, entries, rate_inputs,bonus_rates):
@@ -59,14 +62,17 @@ def update_entries(preset_num, entries, rate_inputs,bonus_rates):
 
     for row in range(3):
         for col in range(3):
-            bet_site = int(entries[row][col]['bet_site'])
-            betted_outcome = int(entries[row][col]['betted_outcome'])
-            bet_rate=float(rate_inputs[bet_site][betted_outcome])
-            bonus_orani=bonus_rates[bet_site]       
+            bet_site = str(entries[row][col]['bet_site'])
+            betted_outcome = str(entries[row][col]['betted_outcome'])
+            bonus_orani=bonus_rates[bet_site]   
+            
+            bet_rate=rate_inputs[bet_site][betted_outcome]
             
             cap_num = entries[row][col]['cap_number']
 
             yatirilacak_tutar = (cap_num * 100) / (bet_rate * (100 + bonus_orani))
+            entries[row][col]['bonus_orani']=bonus_orani
+            entries[row][col]['bet_Rate']=bet_rate
             entries[row][col]['yatirilacak_tutar']=round(yatirilacak_tutar,2)
 
 def calculate_total_deposits(entries, ilk_yatirma ,kalan_cevrim):
@@ -76,26 +82,29 @@ def calculate_total_deposits(entries, ilk_yatirma ,kalan_cevrim):
             yatiran=entries[row][col]['betted_by']
             yatirilacak_tutar=entries[row][col]['yatirilacak_tutar']
             cevrim=entries[row][col]['second_day_YT']
+
+            
             ilk_yatirma[yatiran][bet_site]+=yatirilacak_tutar
             kalan_cevrim[yatiran][bet_site]+=cevrim
 
-    
+
+
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    inputs={}
+    inputs_first={}
     inputs_second={}
     preset_num = None
     preset_num_second=None
-    bonus_rates = {}
+    bonus_rates_first = {}
     bonus_rates_second={}
     match_name='AYIBOĞAN'
     match_name_second='Karlsruhe-Stuttgart'
     winning_outcome= None
     winning_outcome_second=None
-    entries=entries_template
-    entries_second=entries_template
+    entries_first=entries_template
+    entries_second=entries_template_second
     ilk_yatirma=ilk_yatirma_template
     kalan_cevrim=ilk_yatirma_template
 
@@ -105,68 +114,58 @@ def index():
     
 
     if request.method == "POST":
-        #collect first table's inputs in dictionaries so it could be used as placeholder values
-        inputs = {
-            'oflu1': request.form.get('oflu1', ''),
-            'oflu0': request.form.get('oflu0', ''),
-            'oflu2': request.form.get('oflu2', ''),
-            'sex1': request.form.get('sex1', ''),
-            'sex0': request.form.get('sex0', ''),
-            'sex2': request.form.get('sex2', ''),
-            'chad1': request.form.get('chad1', ''),
-            'chad0': request.form.get('chad0', ''),
-            'chad2': request.form.get('chad2', ''),
-        }
         
-        bonus_rates = {
-            'oflu': request.form.get('oflubonus', ''),
-            'sex': request.form.get('sexbonus', ''),
-            'chad': request.form.get('chadbonus', '')
+        bonus_rates_first = {
+            '0': int(request.form.get('oflubonus', '3')),
+            '1': int(request.form.get('sexbonus', '3')),
+            '2': int(request.form.get('chadbonus', '3')),
         }
 
-
-        #second table, same process
-        inputs_second = {
-            'pari1': request.form.get('pari1', ''),
-            'pari0': request.form.get('pari0', ''),
-            'pari2': request.form.get('pari2', ''),
-            'tr1': request.form.get('tr1', ''),
-            'tr0': request.form.get('tr0', ''),
-            'tr2': request.form.get('tr2', ''),
-            'kanyon1': request.form.get('kanyon1', ''),
-            'kanyon0': request.form.get('kanyon0', ''),
-            'kanyon2': request.form.get('kanyon2', ''),
-        }
-        
         bonus_rates_second = {
-            'pari': request.form.get('paribonus', ''),
-            'tr': request.form.get('trbonus', ''),
-            'kanyon': request.form.get('kanyonbonus', '')
+            '0': int(request.form.get('paribonus', '3')),
+            '1': int(request.form.get('trbonus', '3')),
+            '2': int(request.form.get('kanyonbonus', '3')),
         }
 
-        inputs_first = [
-            [request.form.get('oflu0', ''), request.form.get('oflu1', ''), request.form.get('oflu2', '')],
-            [request.form.get('sex0', ''), request.form.get('sex1', ''), request.form.get('sex2', '')],
-            [request.form.get('chad0', ''), request.form.get('chad1', ''), request.form.get('chad2', '')]
-        ]
+        inputs_first = {
+            '0': {
+                '0': float(request.form.get('oflu0', '3')),
+                '1': float(request.form.get('oflu1', '3')),
+                '2': float(request.form.get('oflu2', '3'))
+            },
+            '1': {
+                '0': float(request.form.get('sex0', '3')),
+                '1': float(request.form.get('sex1', '3')),
+                '2': float(request.form.get('sex2', '3'))
+            },
+            '2': {
+                '0': float(request.form.get('chad0', '3')),
+                '1': float(request.form.get('chad1', '3')),
+                '2': float(request.form.get('chad2', '3'))
+            }
+        }
 
-        inputs_second = [
-            [request.form.get('pari0', ''), request.form.get('pari1', ''), request.form.get('pari2', '')],
-            [request.form.get('tr0', ''), request.form.get('tr1', ''), request.form.get('tr2', '')],
-            [request.form.get('kanyon0', ''), request.form.get('kanyon1', ''), request.form.get('kanyon2', '')]
-        ]
+        inputs_second = {
+            '0': {
+                '0': float(request.form.get('pari0', '3')),
+                '1': float(request.form.get('pari1', '3')),
+                '2': float(request.form.get('pari2', '3'))
+            },
+            '1': {
+                '0': float(request.form.get('tr0', '3')),
+                '1': float(request.form.get('tr1', '3')),
+                '2': float(request.form.get('tr2', '3'))
+            },
+            '2': {
+                '0': float(request.form.get('kanyon0', '3')),
+                '1': float(request.form.get('kanyon1', '3')),
+                '2': float(request.form.get('kanyon2', '3'))
+            }
+        }
 
-        bonus_rates_first = [
-            request.form.get('oflubonus', ''),
-            request.form.get('sexbonus', ''),
-            request.form.get('chadbonus', '')
-        ]
 
-        bonus_rates_second = [
-            request.form.get('paribonus', ''),
-            request.form.get('trbonus', ''),
-            request.form.get('kanyonbonus', '')
-        ]
+
+    
 
 
 
@@ -181,38 +180,64 @@ def index():
         winning_outcome = int(request.form.get('winning_outcome', '0'))
         winning_outcome_second= int(request.form.get('winning_outcome_second', '0'))
 
-        #assign both of the match names accordingly
-        match_name=request.form.get('match_name', 'default')
-        match_name_second=request.form.get('match_name_second', 'default')
-    
         
-        update_entries(preset_num, entries,inputs_first,bonus_rates_first)
-        calculate_second_day(entries, winning_outcome)
 
-        update_entries(preset_num_second, entries_second,inputs_second,bonus_rates_second)
-        calculate_second_day(entries_second, winning_outcome_second)
 
-        calculate_total_deposits(entries,ilk_yatirma,kalan_cevrim)
-        calculate_total_deposits(entries_second,ilk_yatirma,kalan_cevrim)
+        update_entries(preset_num, entries_first,inputs_first,bonus_rates_first)
+
+
+        update_entries(preset_num_second,entries_second,inputs_second,bonus_rates_second)
         
+        
+        calculate_second_day(entries_first, winning_outcome)
+        calculate_second_day(entries_second,winning_outcome_second)
 
         
 
-    else:  # If request is GET, reset all variables to their default state
-        inputs={}
-        inputs_second={}
-        preset_num = None
-        preset_num_second=None
-        bonus_rates = {}
-        bonus_rates_second={}
-        match_name='AYIBOĞAN'
-        match_name_second='Karlsruhe-Stuttgart'
-        winning_outcome= None
-        winning_outcome_second=None
-        entries=entries_template
-        entries_second=entries_template
-        ilk_yatirma=ilk_yatirma_template
-        kalan_cevrim=ilk_yatirma_template
+        
+
+        print(inputs_first,bonus_rates_first,winning_outcome,preset_num)
+
+        print("\n\n")
+
+        print(inputs_second,bonus_rates_second,winning_outcome_second,preset_num_second)
+
+        print("\n\n")
+
+        for row in range(3):
+            for col in range(3):
+                print(entries_first[row][col]['bet_Rate'])
+                print(entries_first[row][col]['bonus_orani'])
+                print(entries_first[row][col]['yatirilacak_tutar'])
+                print(entries_first[row][col]['second_day_YT'])
+                print(winning_outcome)
+                     
+
+        print("\n\n")
+
+        for row in range(3):
+            for col in range(3):
+                print(entries_second[row][col]['bet_Rate'])
+                print(entries_second[row][col]['bonus_orani'])
+                print(entries_second[row][col]['yatirilacak_tutar'])
+                print(entries_second[row][col]['betted_outcome'])
+                print(winning_outcome_second)
+        
+
+        
+
+    else:
+    # Page refresh or initial GET request: Reset to default values
+        entries_first = copy.deepcopy(entries_template)
+        entries_second = copy.deepcopy(entries_template_second)
+        ilk_yatirma = copy.deepcopy(ilk_yatirma_template)
+        kalan_cevrim = copy.deepcopy(kalan_cevrim_template)
+        bonus_rates_first = {}
+        bonus_rates_second = {}
+        inputs_first = {}
+        inputs_second = {}
+        winning_outcome = None
+        winning_outcome_second = None
 
         
     # Render the template with current values (no default fallback)
@@ -221,16 +246,15 @@ def index():
                            kalan_cevrim=kalan_cevrim,
                            preset_num=preset_num,
                            preset_num_second=preset_num_second,
-                           bonus_rates=bonus_rates,
+                           bonus_rates_first=bonus_rates_first,
                            bonus_rates_second=bonus_rates_second,
-                           inputs=inputs,
+                           inputs_first=inputs_first,
                            inputs_second=inputs_second,
-                           entries=entries,
+                           entries_first=entries_first,
                            entries_second=entries_second,
                            winning_outcome=winning_outcome,
                            winning_outcome_second=winning_outcome_second,
-                           match_name_second=match_name_second,
-                           match_name=match_name)
+                           )
 
 
 if __name__ == '__main__':
--- erentemplates.py
+++ erentemplates.py
@@ -17,17 +17,37 @@ entries_template = [
     ]
 ]
 
-ilk_yatirma_template = [
-    {'betted_by': 'Mert', 0: 0, 1: 0, 2: 0},
-    {'betted_by': 'Keleş', 0: 0, 1: 0, 2: 0},
-    {'betted_by': 'Yusuf', 0: 0, 1: 0, 2: 0}
+entries_template_second = [
+    [
+        {'bet_site': 0, 'betted_by': 'Mert', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 4000,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 1, 'betted_by': 'Mert', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 4000,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 2, 'betted_by': 'Mert', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 4000,'second_winning_outcome': 0,'katlama_orani':0}
+    ],
+    [
+        {'bet_site': 0, 'betted_by': 'Keleş', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3950,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 1, 'betted_by': 'Keleş', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3950,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 2, 'betted_by': 'Keleş', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3950,'second_winning_outcome': 0,'katlama_orani':0}
+    ],
+    [
+        {'bet_site': 0, 'betted_by': 'Yusuf', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3900,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 1, 'betted_by': 'Yusuf', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3900,'second_winning_outcome': 0,'katlama_orani':0},
+        {'bet_site': 2, 'betted_by': 'Yusuf', 'yatirilacak_tutar': 1.0, 'betted_outcome': 1, 'total_winning': 0, 'second_day_YT': 0, 'bonus_orani': 0, 'bet_Rate': 0, 'cap_number': 3900,'second_winning_outcome':0,
+         'katlama_orani':0}
+    ]
 ]
 
-kalan_cevrim_template = [
-    {'betted_by': 'Mert', 0: 0, 1: 0, 2: 0},
-    {'betted_by': 'Keleş', 0: 0, 1: 0, 2: 0},
-    {'betted_by': 'Yusuf', 0: 0, 1: 0, 2: 0}
-]
+ilk_yatirma_template = {
+    'Mert': {0: 0, 1: 0, 2: 0},
+    'Keleş': {0: 0, 1: 0, 2: 0},
+    'Yusuf': {0: 0, 1: 0, 2: 0}
+}
+
+kalan_cevrim_template = {
+    'Mert': {0: 0, 1: 0, 2: 0},
+    'Keleş': {0: 0, 1: 0, 2: 0},
+    'Yusuf': {0: 0, 1: 0, 2: 0}
+}
+
 
 
 
